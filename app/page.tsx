2fc30568512c06ec0208f56644cc3da5536ce765
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-10 19:28:40 +0530

    add Home component with initial layout and button

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>lets build api</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
