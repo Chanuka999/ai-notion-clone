0d0a28f1979f7e99185d65e14362d7d3e7f86f18
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-11 15:58:59 +0530

    fix: correct service key import and credential initialization in firebase-admin.ts; add User type definition

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,14 +1,18 @@
 import { initializeApp, getApp, getApps, App, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-import serviceKey from "@/service_key.json";
+import serviceKey from "./service_key.json";
 
 let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
-    credential: cert[serviceKey],
+    credential: cert(serviceKey),
   });
 } else {
   app = getApp();
 }
+
+const adminDb = getFirestore(app);
+
+export { app as adminApp, adminDb };
