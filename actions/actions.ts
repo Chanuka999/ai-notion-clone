cff70bd207b291bf674ac12b1584eecb5c19750b
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-11 15:59:17 +0530

    feat: implement createNewDocument action and integrate with NewDocumentButton; add CustomJwtSessionClaims type definition

---
+++ actions/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { adminDb } from "@/firebase-admin";
+import { auth } from "@clerk/nextjs/server";
+
+export async function createNewDocument() {
+  // Manual protection
+  const { userId, sessionClaims } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const docCollectionRef = adminDb.collection("documents");
+  const docRef = await docCollectionRef.add({
+    title: "New Doc",
+  });
+
+  await adminDb
+    .collection("users")
+    .doc(sessionClaims?.email!)
+    .collection("rooms")
+    .doc(docRef.id)
+    .set({
+      userId: sessionClaims?.email,
+      role: "owner",
+      createdAt: new Date(),
+      roomId: docRef.id,
+    });
+
+  return { docId: docRef.id };
+}
--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+"use client";
+import React, { useTransition } from "react";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { createNewDocument } from "@/actions/actions";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 
 const NewDocumentButton = () => {
-  return <Button>New Document</Button>;
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleCreateNewDocument = () => {
+    startTransition(async () => {
+      const { docId } = await createNewDocument();
+      router.push(`doc/${docId}`);
+    });
+  };
+
+  return (
+    <>
+      <SignedIn>
+        <Button onClick={handleCreateNewDocument} disabled={isPending}>
+          {isPending ? "creating" : "New Document"}
+        </Button>
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>{"Sign in to create"}</Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
 };
 
 export default NewDocumentButton;
