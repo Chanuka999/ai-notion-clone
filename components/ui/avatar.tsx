5ad4a2e77021d0e1b6426c9af801170c17f129c2
Chanuka999 <chanukaranditha99@gmail.com>
2026-04-25 01:25:41 +0530

    feat: add Avatar component with fallback and image support

---
+++ components/ui/avatar.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import * as React from "react";
+import {
+  Avatar as RadixAvatar,
+  AvatarImage as RadixAvatarImage,
+  AvatarFallback as RadixAvatarFallback,
+} from "@radix-ui/react-avatar";
+import { cn } from "@/lib/utils";
+
+const Avatar = React.forwardRef(({ className, ...props }, ref) => (
+  <RadixAvatar
+    ref={ref}
+    className={cn(
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      className,
+    )}
+    {...props}
+  />
+));
+Avatar.displayName = "Avatar";
+
+const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
+  <RadixAvatarImage
+    ref={ref}
+    className={cn("aspect-square h-full w-full", className)}
+    {...props}
+  />
+));
+AvatarImage.displayName = "AvatarImage";
+
+const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
+  <RadixAvatarFallback
+    ref={ref}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      className,
+    )}
+    {...props}
+  />
+));
+AvatarFallback.displayName = "AvatarFallback";
+
+export { Avatar, AvatarImage, AvatarFallback };
